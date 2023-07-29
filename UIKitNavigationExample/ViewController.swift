c9bb81a0f32ad73f9d54cda9eb527198c2a541dd
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2023-07-29 20:56:44 +0700

    Initial Commit

---
+++ UIKitNavigationExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitNavigationExample
+//
+//  Created by Jason Rich Darmawan Onggo Putra on 29/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
